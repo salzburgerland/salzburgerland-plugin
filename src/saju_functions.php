f32a024e109710cdcfdb93d7963ba388556a111a
David Riccitelli <david@insideout.io>
2014-11-27 10:22:14 +0200

    fix saju_plugins_url to support relative paths

--- src/saju_functions.php
+++ src/saju_functions.php
@@ -19,10 +19,62 @@ function saju_plugins_url( $url, $path, $plugin ) {
 		return $url;
 	};
 
-	// Set the URL to plugins URL + helixware, in order to support the plugin being symbolic linked.
-	$plugin_url = plugins_url() . '/slt-jukebox/' . $path;
+	// Get the path to the file relative to the plugin root.
+	$rel_path = substr( dirname( $plugin ), strlen( dirname( __FILE__ ) ) ) . '/' . $path;
+
+	// Set the URL to plugins URL in order to support the plugin being symbolic linked.
+	$plugin_url = plugins_url() . '/slt-jukebox/' . $rel_path;
+
+	// Enable this for debugging this function.
+	// saju_write_log( "[ rel path :: $rel_path ][ file :: " . __FILE__  . " ][ url :: $url ][ path :: $path ][ plugin :: $plugin ][ plugins url  :: $plugin_url ]" );
 
 	return $plugin_url;
 }
 
 add_filter( 'plugins_url', 'saju_plugins_url', 10, 3 );
+
+
+/**
+ * Log to the debug.log file.
+ *
+ * @since 1.0.0
+ *
+ * @uses saju_write_log_handler to write the log output.
+ *
+ * @param string|mixed $log The log data.
+ */
+function saju_write_log( $log ) {
+
+	$handler = apply_filters( 'saju_write_log_handler', null );
+
+	$callers         = debug_backtrace();
+	$caller_function = $callers[1]['function'];
+
+	if ( is_null( $handler ) ) {
+		saju_write_log_handler( $log, $caller_function );
+
+		return;
+	}
+
+	call_user_func( $handler, $log, $caller_function );
+}
+
+/**
+ * The default log handler prints out the log.
+ *
+ * @since13.0.0
+ *
+ * @param string|array $log The log data.
+ * @param string $caller The calling function.
+ */
+function saju_write_log_handler( $log, $caller = null ) {
+
+	if ( true === WP_DEBUG ) {
+		if ( is_array( $log ) || is_object( $log ) ) {
+			error_log( "[ $caller ] " . print_r( $log, true ) );
+		} else {
+			error_log( "[ $caller ] " . $log );
+		}
+	}
+
+}
